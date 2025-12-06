db62090c78278ee3f8c3f7f7e35fe7f2a2e2e834
Mohit <mohitt.tech@gmail.com>
2025-12-06 19:46:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,2 @@
 # Generate QR using python
 
-# importing library
-import qrcode
-
-url = input('Enter text or URL :').strip()
-
-# file
-file_path = 'qrcode.png'
-
-qr = qrcode.QRCode()
-qr.add_data(url)
-
-img = qr.make_image()
-img.save(file_path)
-
-print('QR Code generated !')
