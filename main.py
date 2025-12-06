cd2e4cb62d37627bca888a558c1e19df345506e7
Mohit <mohitt.tech@gmail.com>
2025-12-06 19:49:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,12 +9,7 @@ url = input('Enter text or URL: ').strip()
 file_path = 'qrcode.png'
 
 # Create QR code object
-qr = qrcode.QRCode(
-    version=1,  # controls the size of the QR code (1–40)
-    error_correction=qrcode.constants.ERROR_CORRECT_L,  # how much error correction
-    box_size=10,  # size of each box
-    border=4,  # width of border (minimum 4)
-)
+qr = qrcode.QRCode()
 
 # Add data to QR code
 qr.add_data(url)
